e03052759749c696fb2da0a9d4eb71382cec01c9
kfuhs <kevinmfuhs@yahoo.com>
2016-04-16 14:30:44 -0700

    Contact interface created!

--- YoApp/ViewController.swift
+++ YoApp/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
