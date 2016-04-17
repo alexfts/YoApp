58ada082165b3f33f3d1dcf0f9d866451f141999
Alexandra Fetisova <alexandrafetisova@DN525f9k.SUNet>
2016-04-16 19:07:13 -0700

    pulled the old version

--- YoApp/ViewController.swift
+++ YoApp/ViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 import Contacts
 
 class ViewController: UIViewController {
-
+    var name = " "
+    var familyName = " "
+    var phoneNumber = " "
+    
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +28,7 @@ class ViewController: UIViewController {
     func updateWithContact(notification: NSNotification?) {
         name = " "
         familyName = " "
-        phoneNumber = ""
+        phoneNumber = " "
         var tempContact = CNContact()
         
         tempContact = (notification?.userInfo?["contact"]!)! as! CNContact
@@ -42,7 +45,7 @@ class ViewController: UIViewController {
             }
         }
         
-        print(familyName, name, phoneNumber)
+       // print(familyName, name, phoneNumber)
         
     }
 
